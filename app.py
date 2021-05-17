81005957d9b03211e02dbe2ac2ece4b07e61b703
Valeriu Rusu <280167@via.dk>
2021-05-17 15:35:08 +0300

    cors added

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, request
 import requests
 import json
 import time
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/movies', methods=['GET'])
